319c488b6a1cde88a841a515e64d995edf6d6232
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-04-24 22:12:17 -0300

    Adding some data.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>This is my tap one</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
@@ -29,3 +29,5 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+//14:41
