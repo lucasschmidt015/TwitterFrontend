60c85d8de7e4d299e1898e8db92fdc2fcb656d09
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-04-28 01:07:05 -0300

    Started the create new tweet page.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
-import { StyleSheet, FlatList, View } from 'react-native';
+import { StyleSheet, FlatList, View, Pressable } from 'react-native';
 import Tweet from '@/components/Tweet';
 import tweets from '@/assets/data/tweets';
+import { Entypo } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.page}>
       <FlatList data={ tweets } renderItem={ ({ item }) => <Tweet tweet={item} />}/>
+
+      <Link href={'/new-tweet'} asChild>
+        <Pressable style={styles.floatingButton}>
+          <Entypo name="plus" size={45} color="white" />
+        </Pressable>
+      </Link>
     </View>
   );
 }
@@ -14,7 +22,26 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: 'white'
+  },
+  floatingButton: {
+    backgroundColor: '#1C9BF0',
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 15,
+    bottom: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   }
 });
 
-//2:14:10 <---------
+//2:54:36 <---------
---
+++ app/new-tweet.tsx
@@ -0,0 +1,17 @@
+import { View, StyleSheet, Text, Image } from "react-native";
+
+
+
+export default function NewTweet() {
+    return (
+        <View>
+            <Image />
+            <Text>Create new Tweet</Text>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
