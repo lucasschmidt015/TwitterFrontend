32498ff2a77fd2c19c2637a55e3f9f8184ae1234
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-04-29 20:39:18 -0300

    Setting up drawer router.

---
+++ app/(drawer)/bookmarks.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+const Bookmarks = () => {
+    return (
+        <View>
+            <Text>Bookmarks</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Bookmarks;
---
+++ app/(drawer)/twitter-blue.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const TwitterBlue = () => {
+    return (
+        <View>
+            <Text>Twitter-blue</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default TwitterBlue;
